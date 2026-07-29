ba01de944a326843b4b9ce6299509b858e87822b
Ackheron <ackheron@etik.com>
2026-07-29 15:41:28 +0200

    Test extension continu

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+/**
+ * Composant principal de l'application
+ * @returns {JSX.Element} Le titre principal de l'application
+ */
 function App() {
     return <h1>Cookchef</h1>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./assets/styles/index.scss";
 
 createRoot(document.getElementById("root")).render(
+    // Crée une racine React pour la page et affiche le composant App
     <StrictMode>
         <App />
     </StrictMode>,
